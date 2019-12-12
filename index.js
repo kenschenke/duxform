3685ada670b09370743cbdefbaf13ff0f93399c0
Ken Schenke <kschenke@datafreight.com>
2019-12-12 09:13:04 -0600

    Fix index.js in IE 11

--- index.js
+++ index.js
@@ -3,11 +3,22 @@ const DuxInput = require('./lib/DuxInput');
 const DuxAutoComplete = require('./lib/DuxAutoComplete');
 const DuxFormReducer  = require('./lib/reducer');
 
-const { findFormField, getFormData, getFormError, getFormFieldData, getFormFieldError, getFormFieldValue,
-    isFieldValid, isFieldValidOrPristine, isFormValid, isFormValidOrPristine } = require('./lib/helpers');
-const { getAutoCompleteMultiSelectValues, setAutoCompleteSingleSelectValue,
-    setAutoCompleteMultiSelectValues } = require('./lib/actions-autocomplete');
-const { setFormFieldValue } = require('./lib/actions-input');
+const findFormField = require('./lib/helpers').findFormField;
+const getFormData = require('./lib/helpers').getFormData;
+const getFormError = require('./lib/helpers').getFormError;
+const getFormFieldData = require('./lib/helpers').getFormFieldData;
+const getFormFieldError = require('./lib/helpers').getFormFieldError;
+const getFormFieldValue = require('./lib/helpers').getFormFieldValue;
+const isFieldValid = require('./lib/helpers').isFieldValid;
+const isFieldValidOrPristine = require('./lib/helpers').isFieldValidOrPristine;
+const isFormValid = require('./lib/helpers').isFormValid;
+const isFormValidOrPristine = require('./lib/helpers').isFormValidOrPristine;
+
+const getAutoCompleteMultiSelectValues = require('./lib/actions-autocomplete').getAutoCompleteMultiSelectValues;
+const setAutoCompleteSingleSelectValue = require('./lib/actions-autocomplete').setAutoCompleteSingleSelectValue;
+const setAutoCompleteMultiSelectValues = require('./lib/actions-autocomplete').setAutoCompleteMultiSelectValues;
+
+const setFormFieldValue = require('./lib/actions-input').setFormFieldValue;
 
 module.exports = {
     DuxForm: DuxForm,
